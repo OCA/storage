f5a32df46947f539cf93ff34d06c72ba4bc69477
Raph <raph@poir.es>
2017-04-20 17:44:59 +0200

    ajout de la factory

--- storage_image/models/storage_thumbnail.py
+++ storage_image/models/storage_thumbnail.py
@@ -9,6 +9,7 @@ import logging
 
 _logger = logging.getLogger(__name__)
 
+
 class StorageThumbnail(models.Model):
     _name = 'storage.thumbnail'
     _description = 'Storage Thumbnail'
@@ -17,7 +18,6 @@ class StorageThumbnail(models.Model):
     original = fields.Many2one(
         comodel_name='storage.file',
         string='Original file',
-        inverse_name='thumbnail_ids',
         required=True)
 
     size_x = fields.Integer()
@@ -27,3 +27,50 @@ class StorageThumbnail(models.Model):
     # watermarked ?
     # key_frame ?
     to_do = fields.Boolean(help='Mark as to generate from original')
+
+
+class ThumbnailFactory(models.AbstractModel):
+    _name = 'storage.thumbnail.factory'
+
+    def build(self, original, **kwargs):
+        _logger.info('on bulid !')
+        x, y = self.deduce_size(original, **kwargs)
+        backend = self.deduce_backend(original, **kwargs)
+        vals = {
+            'size_x': x,
+            'size_y': y,
+            'to_do': True,
+            'backend_id': backend.id,
+            'name': original.name,
+            'original': original.file_id.id,
+            'res_model': original._name,
+            'res_id': original.id,
+        }
+        _logger.info(vals)
+        self.env['storage.thumbnail'].create(vals)
+
+    def deduce_backend(self, original, **kwargs):
+        # on met kwargs ici car on peut avoir des règles metiers
+
+        backends = self.env['storage.backend'].search([])
+        return backends[0]  # par defaut on prends le premier
+
+        # autres idées
+        # on a une config
+        backend_name = self.env['ir.config_parameter'].get_param(
+            'storage.thumbnail.backend')
+        return self.env['storage.backend'].search(['name', '=', backend_name])
+
+        # ne servir du CDN qu'a partir d'une certaine taille
+        if kwargs['size_x'] == 90:
+            return 'local_backend'
+        else:
+            return 'S3'
+        return True
+
+    def deduce_size(self, original, size_x, size_y, **kwargs):
+        # objectifs: renvoyer une val par defaut a size_x
+        # et s'assurer un minimum de coherence
+        # TODO: return min(size_x, origina.size_x)
+        # size_x = size_x or original.size_x
+        return size_x, size_y
