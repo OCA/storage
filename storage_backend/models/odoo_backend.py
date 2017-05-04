ab1d702bd6e7e4607a2d8ba499ed1f841016b4c6
Raph <raph@poir.es>
2017-05-04 16:46:54 +0200

    Pylinting

--- storage_backend/models/odoo_backend.py
+++ storage_backend/models/odoo_backend.py
@@ -3,9 +3,8 @@
 # @author Sébastien BEAU <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import api, fields, models
+from openerp import fields, models
 import hashlib
-from fs.osfs import OSFS
 import logging
 logger = logging.getLogger(__name__)
 
