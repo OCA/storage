80704b0290a96af6f588a4f6e00193ac6cd88036
Raph <raph@poir.es>
2017-05-04 18:04:20 +0200

    Fix import order

--- storage_image/models/__init__.py
+++ storage_image/models/__init__.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from . import partner
-from . import product
 from . import storage_image
 from . import storage_image_wizard
 from . import storage_image_factory
 from . import storage_thumbnail
 from . import storage_thumbnail_wizard
 from . import storage_thumbnail_factory
+
+from . import partner
+from . import product
