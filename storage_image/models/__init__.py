6d1458797988b6c9ab93d8375b4a6b59bfc0d1a4
Raph <raph@poir.es>
2017-05-04 16:27:08 +0200

    Refactor

--- storage_image/models/__init__.py
+++ storage_image/models/__init__.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from . import storage_image
-from . import storage_thumbnail
 from . import partner
 from . import product
+from . import storage_image
 from . import storage_image_wizard
+from . import storage_image_factory
+from . import storage_thumbnail
 from . import storage_thumbnail_wizard
+from . import storage_thumbnail_factory
