b6797d507c1ea600b35e1630571db4ad5bbf9bb3
Sébastien BEAU <sebastien.beau@akretion.com>
2017-05-22 13:44:41 +0200

    [REF] continue refactoring, remove inheriting ir.attachment as the write/create was inherited and this was adding issue, use a computed field to store the field into the storage backend

--- storage_file/models/storage_file.py
+++ storage_file/models/storage_file.py
@@ -4,37 +4,46 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import api, fields, models
+from openerp.exceptions import Warning as UserError
 import urllib, base64
 import logging
 import os
+import mimetypes
+import hashlib
 _logger = logging.getLogger(__name__)
+
+try:
+    import cStringIO as StringIO
+except ImportError:
+    import StringIO
 
 
 class StorageFile(models.Model):
     _name = 'storage.file'
     _description = 'Storage File'
-    _inherit = 'ir.attachment'
 
-    # redefinition du field
-    url = fields.Char(help="HTTP accessible path for odoo backend to the file")
-    private_path = fields.Char(help='Location for backend, may be relative')
-    public_url = fields.Char(
-        compute='_compute_url',
-        help='Public url like CDN')  # ou path utile ?
+    name = fields.Char(
+        required=True,
+        select=True)
     backend_id = fields.Many2one(
         'storage.backend',
         'Storage',
+        select=True,
         required=True)
-
-    res_model = fields.Char(readonly=False)
-    res_id = fields.Integer(readonly=False)
-
-    # forward compliency to v9 and v10
+    url = fields.Char(help="HTTP accessible path for odoo backend to the file")
+    private_path = fields.Char(help='Location for backend, may be relative')
+    res_model = fields.Char(
+        readonly=False,
+        select=True)
+    res_id = fields.Integer(
+        readonly=False,
+        select=True)
+    file_size = fields.Integer('File Size')
     checksum = fields.Char(
-        "Checksum/SHA1", size=40, select=True, readonly=True)
-    mimetype = fields.Char('Mime Type', readonly=True)
-    index_content = fields.Char('Indexed Content', readonly=True)
-    public = fields.Boolean('Is public document')
+        "Checksum/SHA1",
+        size=40,
+        select=True,
+        readonly=True)
 
     filename = fields.Char(
         "Filename without extension",
@@ -44,11 +53,34 @@ class StorageFile(models.Model):
         "Extension",
         compute='_compute_extract_filename',
         store=True)
-
+    mimetype = fields.Char(
+        "Mime Type",
+        compute='_compute_extract_filename',
+        store=True)
     datas = fields.Binary(
-        help="The file",
+        help="Datas",
+        inverse='_inverse_datas',
         compute='_compute_datas',
         store=False)  #
+
+    def _prepare_meta_for_file(self, datas):
+        return {
+            'url': self.backend_id.get_public_url(self.name),
+            'checksum': hashlib.sha1(datas).hexdigest(),
+            'file_size': len(datas),
+            }
+
+    @api.multi
+    def _inverse_datas(self):
+        for record in self:
+            b_decoded = base64.b64decode(record.datas)
+            self.backend_id.store(
+                record.name,
+                b_decoded,
+                is_public=True,
+                is_base64=False)
+            vals = record._prepare_meta_for_file(b_decoded)
+            record.write(vals)
 
     @api.multi
     def _compute_datas(self):
@@ -59,19 +91,9 @@ class StorageFile(models.Model):
                 _logger.error('Image %s not found', rec.url)
                 rec.datas = None
 
-    @api.model
-    def create(self, vals):
-        _logger.info('dans parent, normalement on devrait faire le store ici')
-        return super(StorageFile, self).create(vals)
-
-    @api.depends('backend_id')
-    def _compute_url(self):
-        # attention peut être appelé n'importe quand
-        _logger.info('compute_url du parent')
-        for rec in self:
-            rec.public_url = rec.backend_id.get_public_url(rec)
-
     @api.depends('name')
     def _compute_extract_filename(self):
         for rec in self:
             rec.filename, rec.extension = os.path.splitext(rec.name)
+            mime, enc = mimetypes.guess_type(rec.name)
+            rec.mimetype = mime
