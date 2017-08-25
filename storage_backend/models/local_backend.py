0b11a54971e356bc91607bc90b5dd90fb20c1b97
Sébastien BEAU <sebastien.beau@akretion.com>
2017-08-25 13:21:18 +0200

    [FIX] jail local  storage inside the directory <path_to_filestore>/storage, this will avoid reading/writing unwanted file

--- storage_backend/models/local_backend.py
+++ storage_backend/models/local_backend.py
@@ -6,6 +6,7 @@
 import base64
 import logging
 import os
+import re
 from openerp import fields, models
 logger = logging.getLogger(__name__)
 
@@ -21,23 +22,34 @@ class FileStoreStorageBackend(models.Model):
     filestore_base_path = fields.Char(
         sparse="data")
 
+    def _fullpath(self, name):
+        """This will build the full path for the file, we force to
+        store the data inside the filestore in the directory 'storage".
+        Becarefull if you implement your own custom path, end user
+        should never be able to write or read unwanted filesystem file"""
+        # sanitize base_path
+        base_path = re.sub('[.]', '', self.filestore_base_path).strip('/\\')
+        # sanitize name
+        name = name.strip('/\\')
+        return os.path.join(
+            self.env['ir.attachment']._filestore(), 'storage', base_path, name)
+
     def _filestore_store(self, name, datas, is_public=False):
-        # TODO: refactorer, ça marche plus vraiment
-        # enregistre le binary la où on lui dit
-        # renvois l'objet en question
-        full_path = os.path.join(self.filestore_base_path, name)
+        full_path = self._fullpath(name)
+        dirname = os.path.dirname(full_path)
+        if not os.path.isdir(dirname):
+            os.makedirs(dirname)
+        logger.debug('Backend Storage: Write file %s to filestore', full_path)
         with open(full_path, "wb") as my_file:
             my_file.write(datas)
         return name
 
     def _filestoreget_public_url(self, name):
-        # TODO faire mieux
-        logger.info('get_public_url')
         return os.path.join(self.filestore_public_base_url, name)
 
     def _filestoreretrieve_datas(self, name):
-        logger.info('return base64 of a file')
-        full_path = os.path.join(self.filestore_base_path, name)
+        logger.debug('Backend Storage: Read file %s from filestore', name)
+        full_path = self._fullpath(name)
         with open(full_path, "b") as my_file:
             datas = my_file.read()
         return datas and base64.b64encode(datas) or False
