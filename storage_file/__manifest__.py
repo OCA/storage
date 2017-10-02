75d77ac9d30afe2a358a08df162dc49aed508594
David Beal <david.beal@akretion.com>
2017-10-02 17:30:34 +0200

    FIX after merge base branch [skip ci]

--- storage_file/__manifest__.py
+++ storage_file/__manifest__.py
@@ -6,13 +6,13 @@
 {
     "name": "Storage File",
     "summary": "Storage file in storage backend",
-    "version": "8.0.1.0.0",
+    "version": "10.0.1.0.0",
     "category": "Uncategorized",
     "website": "www.akretion.com",
     "author": " Akretion",
     "license": "AGPL-3",
     "application": False,
-    'installable': False,
+    'installable': True,
     "external_dependencies": {
         "python": [],
         "bin": [],
