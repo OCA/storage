c7e51ec1fa60021666a4e363e89fa138a09142cf
Sébastien BEAU <sebastien.beau@akretion.com>
2018-04-09 17:37:07 +0200

    [REF] start refactoring storage_image, split storage_image in two module, storage_image and storage_thumbnail add a generic mixing for thumbnail owner

---
+++ storage_thumbnail/models/storage_thumbnail.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Akretion (http://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from odoo import api, fields, models
+from odoo.tools import image_resize_image
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class StorageThumbnail(models.Model):
+    _name = 'storage.thumbnail'
+    _description = 'Storage Thumbnail'
+    _inherits = {'storage.file': 'file_id'}
+
+    size_x = fields.Integer("weight")
+    size_y = fields.Integer("height")
+    file_id = fields.Many2one(
+        'storage.file',
+        'File',
+        required=True,
+        ondelete='cascade')
+    res_model = fields.Char(
+        readonly=False,
+        index=True)
+    res_id = fields.Integer(
+        readonly=False,
+        index=True)
+
+    def _prepare_thumbnail(self, image, size_x, size_y):
+        return {
+            'datas': self._resize(image, size_x, size_y),
+            'res_model': image._name,
+            'res_id': image.id,
+            'name': '%s_%s_%s%s' % (
+                image.filename, size_x, size_y, image.extension),
+            'size_x': size_x,
+            'size_y': size_y,
+        }
+
+    def _resize(self, image, size_x, size_y):
+        return image_resize_image(image.datas, size=(size_x, size_y))
+
+    def _create_thumbnail(self, image, size_x, size_y):
+        vals = self._prepare_thumbnail(image, size_x, size_y)
+        return self.create(vals)
+
+    def _get_backend_id(self):
+        """Choose the correct backend.
+
+        By default : it's the one configured as ir.config_parameter
+        Overload this method if you need something more powerfull
+        """
+        return int(self.env['ir.config_parameter'].get_param(
+            'storage.thumbnail.backend_id'))
+
+    @api.model
+    def create(self, vals):
+        vals['backend_id'] = self._get_backend_id()
+        return super(StorageThumbnail, self).create(vals)
