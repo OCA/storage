6c1fb48d3bcc9d351ce70b00a8c460224b3081ed
Sébastien BEAU <sebastien.beau@akretion.com>
2018-04-09 17:37:07 +0200

    [REF] start refactoring storage_image, split storage_image in two module, storage_image and storage_thumbnail add a generic mixing for thumbnail owner

---
+++ storage_thumbnail/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Akretion (http://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Storage Thumbnail",
+    "summary": "Abstract module that add the possibility to have thumbnail",
+    "version": "10.0.1.0.0",
+    "category": "Storage",
+    "website": "www.akretion.com",
+    "author": " Akretion",
+    "license": "AGPL-3",
+    'installable': True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "storage_file",
+    ],
+    "data": [
+        'data/ir_parameter.xml',
+        'views/storage_thumbnail_view.xml',
+        'security/ir.model.access.csv',
+    ],
+    "demo": [
+    ],
+    "qweb": [
+    ]
+}
