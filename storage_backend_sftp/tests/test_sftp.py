7da011ff105768a94ff4996dc9890500f6492e23
Sébastien BEAU <sebastien.beau@akretion.com>
2018-04-11 13:17:44 +0200

    [REF] split sftp backend in a separated module

---
+++ storage_backend_sftp/tests/test_sftp.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Akretion (http://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+# pylint: disable=missing-manifest-dependency
+# disable warning on 'vcr' missing in manifest: this is only a dependency for
+# dev/tests
+
+from odoo.addons.storage_backend.tests.common import Common
+import os
+import base64
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class SftpCase(Common):
+
+    def setUp(self):
+        super(SftpCase, self).setUp()
+        self.backend = self.env.ref('storage_backend.default_storage_backend')
+        self.backend.write({
+            'backend_type': 'sftp',
+            'sftp_login': 'foo',
+            'sftp_password': 'pass',
+            'sftp_server': os.environ.get('SFTP_HOST', 'localhost'),
+            'sftp_port': os.environ.get('SFTP_PORT', '2222'),
+            'directory_path': 'upload',
+            })
+
+    def test_00_setting_and_reading_data_from_root(self):
+        self.backend.store(
+            self.filename, self.filedata,
+            is_base64=False, mimetype=u'text/plain')
+        data = self.backend.retrieve_data(self.filename)
+        self.assertEqual(base64.b64decode(data), self.filedata)
+
+    def test_10_setting_and_reading_data_from_directory(self):
+        self.backend.directory_path = 'upload/subdirectory/here'
+        self.backend.store(
+            self.filename, self.filedata,
+            is_base64=False, mimetype=u'text/plain')
+        data = self.backend.retrieve_data(self.filename)
+        self.assertEqual(base64.b64decode(data), self.filedata)
