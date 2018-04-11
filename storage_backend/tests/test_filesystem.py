30499074f39d65df0207c6c0d280fe041c32a83f
Sébastien BEAU <sebastien.beau@akretion.com>
2018-04-11 19:42:42 +0200

    [REF] refactor test in order to use the same test between the different backend

--- storage_backend/tests/test_filesystem.py
+++ storage_backend/tests/test_filesystem.py
@@ -3,17 +3,13 @@
 # @author Sébastien BEAU <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from .common import Common
+from .common import Common, GenericStoreCase
 from odoo.tools import config
 import os
 import base64
 
 
-class FileStoreCase(Common):
-
-    def setUp(self):
-        super(FileStoreCase, self).setUp()
-        self.backend = self.env.ref('storage_backend.default_storage_backend')
+class FileSystemCase(Common, GenericStoreCase):
 
     def _get_filepath(self, filename):
         return os.path.join(
@@ -22,19 +18,15 @@ class FileStoreCase(Common):
             self.backend.directory_path or '',
             filename)
 
-    def test_00_setting_and_getting_data_from_root(self):
-        self.backend.add_b64_data(self.filename, self.filedata)
+    def _check_file_on_filestore(self):
         filepath = self._get_filepath(self.filename)
         data = open(filepath, 'r').read()
         self.assertEqual(data, base64.b64decode(self.filedata))
-        data = self.backend.get_b64_data(self.filename)
-        self.assertEqual(data, self.filedata)
 
-    def test_00_setting_and_getting_data_from_dir(self):
-        self.backend.directory_path = 'subdirectory/here'
-        self.backend.add_b64_data(self.filename, self.filedata)
-        filepath = self._get_filepath(self.filename)
-        data = open(filepath, 'r').read()
-        self.assertEqual(data, base64.b64decode(self.filedata))
-        data = self.backend.get_b64_data(self.filename)
-        self.assertEqual(data, self.filedata)
+    def test_setting_and_getting_data_from_root(self):
+        super(FileSystemCase, self).test_setting_and_getting_data_from_root()
+        self._check_file_on_filestore()
+
+    def test_setting_and_getting_data_from_dir(self):
+        super(FileSystemCase, self).test_setting_and_getting_data_from_dir()
+        self._check_file_on_filestore()
