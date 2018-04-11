ce9de1795575b955196f5c58a4a915a92d71c5a8
Sébastien BEAU <sebastien.beau@akretion.com>
2018-04-11 19:42:42 +0200

    [REF] rename method store and retrieve by more explicit method add/get with the specifiation of the type of file binary or base64

--- storage_backend/tests/common.py
+++ storage_backend/tests/common.py
@@ -4,11 +4,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo.addons.component.tests.common import TransactionComponentCase
+import base64
 
 
 class Common(TransactionComponentCase):
 
     def setUp(self):
         super(Common, self).setUp()
-        self.filedata = 'This is a simple file'
+        self.filedata = base64.b64encode('This is a simple file')
         self.filename = 'test_file.txt'
