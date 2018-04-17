1fa337047470cc942b4f46ddb08833724a2ce5b9
Sébastien BEAU <sebastien.beau@akretion.com>
2018-04-17 17:53:09 +0200

    [REF] rename module and add filter on attribute values

--- storage_image_product/__manifest__.py
+++ storage_image_product/__manifest__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Image Product",
+    "name": "Storage Image Product",
     "summary": "Link images to products and categories",
     "version": "10.0.1.0.0",
     "category": "Storage",
@@ -14,7 +14,8 @@
     'installable': True,
     "depends": [
         "storage_image",
-        'product',
+        "product",
+        "web_m2x_options",
     ],
     "data": [
         "views/product_template.xml",
--- storage_image_product/models/product_image.py
+++ storage_image_product/models/product_image.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
-from odoo import fields, models
+from odoo import api, fields, models
 import logging
 
 _logger = logging.getLogger(__name__)
@@ -20,6 +20,11 @@ class ProductImage(models.Model):
         'product.attribute.value',
         string='Attributes'
     )
+    available_attribute_value_ids = fields.Many2many(
+        'product.attribute.value',
+        string='Attributes',
+        compute="_compute_available_attribute"
+    )
     product_tmpl_id = fields.Many2one(
         'product.template',
     )
@@ -27,3 +32,10 @@ class ProductImage(models.Model):
     image_name = fields.Char(related='image_id.name')
     # for kanban view
     image_url = fields.Char(related='image_id.image_medium_url')
+
+    @api.depends('image_id', 'product_tmpl_id.attribute_line_ids.value_ids')
+    def _compute_available_attribute(self):
+        # the depend on 'image_id' only added for triggering the onchange
+        for record in self:
+            record.available_attribute_value_ids =\
+                record.product_tmpl_id.mapped('attribute_line_ids.value_ids')
