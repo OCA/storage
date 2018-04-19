dd2ecd83b3c6c28ede622c13a1f3cb67f76160fc
Sébastien BEAU <sebastien.beau@akretion.com>
2018-04-19 12:14:03 +0200

    [IMP] improve case test, ad logo on product and check it

--- storage_image_product/tests/test_product_image.py
+++ storage_image_product/tests/test_product_image.py
@@ -33,18 +33,22 @@ class ProductImageCase(TransactionComponentCase):
         self.product_a = self.env.ref('product.product_product_4')
         self.product_b = self.env.ref('product.product_product_4b')
         self.product_c = self.env.ref('product.product_product_4c')
+        self.logo_image = self._create_storage_image('logo-image.jpg')
         self.white_image = self._create_storage_image('white-image.jpg')
         self.black_image = self._create_storage_image('black-image.jpg')
 
     def test_available_attribute_value(self):
+        # The template have already 5 attribute values
+        # see demo data of ipad
         image = self.env['product.image'].new({
             'product_tmpl_id': self.template.id})
         self.assertEqual(len(image.available_attribute_value_ids), 5)
 
     def test_add_image_for_all_variant(self):
+        self.assertEqual(len(self.product_a.variant_image_ids), 0)
         image = self.env['product.image'].create({
             'product_tmpl_id': self.template.id,
-            'image_id': self.white_image.id,
+            'image_id': self.logo_image.id,
             })
         self.assertEqual(self.product_a.variant_image_ids, image)
         self.assertEqual(self.product_b.variant_image_ids, image)
@@ -57,11 +61,17 @@ class ProductImageCase(TransactionComponentCase):
             'attribute_value_ids': [(6, 0, [self.env.ref(
                 'product.product_attribute_value_3').id])]
             })
+        # White product should have the image
         self.assertEqual(self.product_a.variant_image_ids, image)
-        self.assertEqual(len(self.product_b.variant_image_ids), 0)
         self.assertEqual(self.product_c.variant_image_ids, image)
+        # Black product should not have the image
+        self.assertEqual(len(self.product_b.variant_image_ids), 0)
 
     def test_add_image_for_white_and_black_variant(self):
+        logo = self.env['product.image'].create({
+            'product_tmpl_id': self.template.id,
+            'image_id': self.logo_image.id,
+            })
         image_wh = self.env['product.image'].create({
             'product_tmpl_id': self.template.id,
             'image_id': self.white_image.id,
@@ -74,6 +84,8 @@ class ProductImageCase(TransactionComponentCase):
             'attribute_value_ids': [(6, 0, [self.env.ref(
                 'product.product_attribute_value_4').id])]
             })
-        self.assertEqual(self.product_a.variant_image_ids, image_wh)
-        self.assertEqual(self.product_b.variant_image_ids, image_bk)
-        self.assertEqual(self.product_c.variant_image_ids, image_wh)
+        # White product should have the white image and the logo
+        self.assertEqual(self.product_a.variant_image_ids, image_wh + logo)
+        self.assertEqual(self.product_c.variant_image_ids, image_wh + logo)
+        # Black product should have the black image and the logo
+        self.assertEqual(self.product_b.variant_image_ids, image_bk + logo)
