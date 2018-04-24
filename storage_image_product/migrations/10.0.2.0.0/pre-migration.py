fc737d59170250438d5b51d73e79938c3828db86
François Honoré <francois.honore@acsone.eu>
2018-04-24 13:30:51 +0200

    [FIX] Fix an issue about model name (product.image) conflict when website_sale is installed (this standard module have a same model)
    Update security

---
+++ storage_image_product/migrations/10.0.2.0.0/pre-migration.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+def migrate(cr, version):
+    """
+    Change of this 10.0.2 version:
+    - Rename product.image into product.image.relation because website_sale
+    module already have the same model;
+    - Apply the same logic on the category.image (renamed into
+    category.image.relation).
+    If this module is installed, it means that website_sale module is not
+    installed (otherwise, we already have an issue).
+    So this migration script doesn't check if website_sale is installed.
+    :param cr: database cursor
+    :param version: str
+    :return:
+    """
+    if not version:
+        return
+    query_product = "ALTER TABLE product_image " \
+                    "RENAME TO product_image_relation;"
+    query_category = "ALTER TABLE category_image " \
+                     "RENAME TO category_image_relation;"
+    cr.execute(query_product)
+    cr.execute(query_category)
