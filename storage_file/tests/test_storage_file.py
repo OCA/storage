9c2598d0dcde46d0f424aba5d3e57bc7bbccf57d
Benoit <benoit.guillot@akretion.com>
2019-04-10 18:27:35 +0200

    [FIX] clean with pre-commit and pep 8

--- storage_file/tests/test_storage_file.py
+++ storage_file/tests/test_storage_file.py
@@ -3,53 +3,57 @@
 # @author Sébastien BEAU <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.addons.component.tests.common import TransactionComponentCase
 import base64
+
 import urlparse
-from odoo.exceptions import UserError, AccessError
+from odoo.addons.component.tests.common import TransactionComponentCase
+from odoo.exceptions import AccessError, UserError
 
 
 class StorageFileCase(TransactionComponentCase):
-
     def setUp(self):
         super(StorageFileCase, self).setUp()
-        self.backend = self.env.ref('storage_backend.default_storage_backend')
-        data = 'This is a simple file'
+        self.backend = self.env.ref("storage_backend.default_storage_backend")
+        data = "This is a simple file"
         self.filesize = len(data)
         self.filedata = base64.b64encode(data)
-        self.filename = 'test of my_file.txt'
+        self.filename = "test of my_file.txt"
 
     def _create_storage_file(self):
-        return self.env['storage.file'].create({
-            'name': self.filename,
-            'backend_id': self.backend.id,
-            'data': self.filedata,
-            })
+        return self.env["storage.file"].create(
+            {
+                "name": self.filename,
+                "backend_id": self.backend.id,
+                "data": self.filedata,
+            }
+        )
 
     def test_create_and_read_served_by_odoo(self):
         stfile = self._create_storage_file()
         self.assertEqual(stfile.data, self.filedata)
-        self.assertEqual(stfile.mimetype, u'text/plain')
-        self.assertEqual(stfile.extension, u'.txt')
-        self.assertEqual(stfile.filename, u'test of my_file')
+        self.assertEqual(stfile.mimetype, u"text/plain")
+        self.assertEqual(stfile.extension, u".txt")
+        self.assertEqual(stfile.filename, u"test of my_file")
         self.assertEqual(
-            stfile.relative_path, u'test-of-my_file-%s.txt' % stfile.id)
+            stfile.relative_path, u"test-of-my_file-%s.txt" % stfile.id
+        )
         url = urlparse.urlparse(stfile.url)
         self.assertEqual(
-            url.path,
-            "/web/content/storage.file/%s/data" % stfile.id)
+            url.path, "/web/content/storage.file/%s/data" % stfile.id
+        )
         self.assertEqual(stfile.file_size, self.filesize)
 
     def test_create_store_with_hash(self):
-        self.backend.filename_strategy = 'hash'
+        self.backend.filename_strategy = "hash"
         stfile = self._create_storage_file()
         self.assertEqual(stfile.data, self.filedata)
-        self.assertEqual(stfile.mimetype, u'text/plain')
-        self.assertEqual(stfile.extension, u'.txt')
-        self.assertEqual(stfile.filename, u'test of my_file')
+        self.assertEqual(stfile.mimetype, u"text/plain")
+        self.assertEqual(stfile.extension, u".txt")
+        self.assertEqual(stfile.filename, u"test of my_file")
         self.assertEqual(
             stfile.relative_path,
-            u'13/1322d9ccb3d257095185b205eadc9307aae5dc84')
+            u"13/1322d9ccb3d257095185b205eadc9307aae5dc84",
+        )
 
     def test_missing_name_strategy(self):
         self.backend.filename_strategy = None
@@ -57,31 +61,31 @@ class StorageFileCase(TransactionComponentCase):
             self._create_storage_file()
 
     def test_create_and_read_served_by_external(self):
-        self.backend.write({
-            'served_by': 'external',
-            'base_url': 'https://cdn.example.com',
-            })
+        self.backend.write(
+            {"served_by": "external", "base_url": "https://cdn.example.com"}
+        )
         stfile = self._create_storage_file()
         self.assertEqual(stfile.data, self.filedata)
         self.assertEqual(
             stfile.url,
-            'https://cdn.example.com/test-of-my_file-%s.txt' % stfile.id)
+            "https://cdn.example.com/test-of-my_file-%s.txt" % stfile.id,
+        )
         self.assertEqual(stfile.file_size, self.filesize)
 
     def test_read_bin_size(self):
         stfile = self._create_storage_file()
         self.assertEqual(
-            stfile.with_context(bin_size=True).data,
-            '21.00 bytes')
+            stfile.with_context(bin_size=True).data, "21.00 bytes"
+        )
 
     def test_cannot_update_data(self):
         stfile = self._create_storage_file()
-        data = base64.b64encode('This is different data')
+        data = base64.b64encode("This is different data")
         with self.assertRaises(UserError):
-            stfile.write({'data': data})
+            stfile.write({"data": data})
 
         # check that the file have been not modified
-        self.assertEqual(stfile.read()[0]['data'], self.filedata)
+        self.assertEqual(stfile.read()[0]["data"], self.filedata)
 
     def test_unlink(self):
         # Do not commit during the test
@@ -98,11 +102,14 @@ class StorageFileCase(TransactionComponentCase):
         self.assertIn(relative_path, backend._list())
 
         # Run the method to clean the storage.file
-        self.env['storage.file']._clean_storage_file()
+        self.env["storage.file"]._clean_storage_file()
 
         # Check that the file is deleted
-        files = self.env['storage.file'].with_context(
-            active_test=False).search([('id', '=', stfile.id)])
+        files = (
+            self.env["storage.file"]
+            .with_context(active_test=False)
+            .search([("id", "=", stfile.id)])
+        )
         self.assertEqual(len(files), 0)
         self.assertNotIn(relative_path, backend._list())
 
@@ -123,9 +130,8 @@ class StorageFileCase(TransactionComponentCase):
         # Public user used on the controller when authentication is 'public'
         public_user = self.env.ref("base.public_user")
         env = self.env(user=public_user)
-        storage_file_public = env[storage_file._name].browse(storage_file.ids)
         with self.assertRaises(AccessError):
-            storage_file_public.name
+            env[storage_file._name].browse(storage_file.ids).name
         return True
 
     def test_public_access2(self):
@@ -139,9 +145,7 @@ class StorageFileCase(TransactionComponentCase):
         :return: bool
         """
         storage_file = self._create_storage_file()
-        storage_file.backend_id.write({
-            'is_public': True,
-        })
+        storage_file.backend_id.write({"is_public": True})
         self.assertTrue(storage_file.backend_id.is_public)
         # Public user used on the controller when authentication is 'public'
         public_user = self.env.ref("base.public_user")
