18bdde2de7cd58375c21545d57a6380bf3b3a206
Benoit <benoit.guillot@akretion.com>
2019-04-10 18:27:35 +0200

    [FIX] clean with pre-commit and pep 8

--- storage_backend/tests/test_filesystem.py
+++ storage_backend/tests/test_filesystem.py
@@ -3,16 +3,16 @@
 # @author Sébastien BEAU <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from .common import Common, GenericStoreCase
 from odoo.exceptions import AccessError
+
+from .common import Common, GenericStoreCase
 
 
 class FileSystemCase(Common, GenericStoreCase):
-
     def test_demo_user_can_not_use_storage(self):
         # Demo user do not have the access right
-        self.user = self.env.ref('base.user_demo')
+        self.user = self.env.ref("base.user_demo")
         self.env = self.env(user=self.user)
-        self.backend = self.env.ref('storage_backend.default_storage_backend')
+        self.backend = self.env.ref("storage_backend.default_storage_backend")
         with self.assertRaises(AccessError):
             self._test_setting_and_getting_data()
