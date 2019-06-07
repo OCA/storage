46fcde999fc59f40fd65e312d1d9b2fbcd2dfb45
Denis Roussel <denis.roussel@acsone.eu>
2019-06-07 11:55:47 +0200

    [12.0] storage*: Make installable False

--- storage_file/__manifest__.py
+++ storage_file/__manifest__.py
@@ -12,7 +12,7 @@
     "author": " Akretion",
     "license": "AGPL-3",
     "application": False,
-    "installable": True,
+    "installable": False,
     "external_dependencies": {"python": ["slugify"], "bin": []},
     "depends": ["storage_backend"],
     "data": [
