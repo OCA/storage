3be22a5cf08adc4d10d53b0ae20e6d0deeac2c82
Thomas Binsfeld <thomas.binsfeld@acsone.eu>
2019-07-09 10:20:36 +0200

    [IMP] Storage Image Product: default value of Apply On (image tag) Add a default value for the field 'Apply On', on image tags when creating them on the fly from category.image.relation or product.image.relation. The default value is 'product' when coming from product.image.relation and 'category' when coming from category.image.relation. Still no default value is provided when there's no active model matching one of these two in the context.

--- storage_image_product/models/image_tag.py
+++ storage_image_product/models/image_tag.py
@@ -3,14 +3,26 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ImageTag(models.Model):
     _name = "image.tag"
     _description = "Image Tag"
 
+    @api.model
+    def _get_default_apply_on(self):
+        active_model = self.env.context.get("active_model")
+        return (
+            "product"
+            if active_model == "product.image.relation"
+            else "category"
+            if active_model == "category.image.relation"
+            else False
+        )
+
     name = fields.Char()
     apply_on = fields.Selection(
-        selection=[("product", "Product"), ("category", "Category")]
+        selection=[("product", "Product"), ("category", "Category")],
+        default=lambda self: self._get_default_apply_on(),
     )
