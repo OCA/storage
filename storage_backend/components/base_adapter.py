638fe8c54871478c49c577b2c62ef2a768a55ba9
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2019-10-01 22:35:35 +0200

    pre-commit, black, isort
    [UPD] Update storage_backend.pot

--- storage_backend/components/base_adapter.py
+++ storage_backend/components/base_adapter.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Akretion (http://www.akretion.com).
 # @author Sébastien BEAU <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -14,9 +13,7 @@ class BaseStorageAdapter(AbstractComponent):
 
     def _fullpath(self, relative_path):
         if self.collection.directory_path:
-            return os.path.join(
-                self.collection.directory_path or "", relative_path
-            )
+            return os.path.join(self.collection.directory_path or "", relative_path)
         else:
             return relative_path
 
