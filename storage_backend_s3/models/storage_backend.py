78f8866bab54bc1e2022426eaa6645f578a3ab0c
Simone Orsi <simahawk@gmail.com>
2019-10-14 15:28:25 +0200

    [MIG] storage_backend_s3: Migration to 12.0

--- storage_backend_s3/models/storage_backend.py
+++ storage_backend_s3/models/storage_backend.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Akretion (http://www.akretion.com).
 # @author Sébastien BEAU <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -18,18 +17,18 @@ except ImportError as err:  # pragma: no cover
 class StorageBackend(models.Model):
     _inherit = "storage.backend"
 
-    def _get_aws_region(self):
+    backend_type = fields.Selection(selection_add=[("amazon_s3", "Amazon S3")])
+    aws_bucket = fields.Char(string="Bucket")
+    aws_access_key_id = fields.Char(string="Access Key ID")
+    aws_secret_access_key = fields.Char(string="Secret Access Key")
+    aws_region = fields.Selection(
+        selection='_selection_aws_region', string="Region"
+    )
+    aws_cache_control = fields.Char(default="max-age=31536000, public")
+
+    def _selection_aws_region(self):
         session = boto3.session.Session()
         return [
             (region, region.replace("-", " ").capitalize())
             for region in session.get_available_regions("s3")
         ]
-
-    backend_type = fields.Selection(selection_add=[("amazon_s3", "Amazon S3")])
-    aws_bucket = fields.Char(sparse="data", string="Bucket")
-    aws_access_key_id = fields.Char(sparse="data", string="Access Key ID")
-    aws_secret_access_key = fields.Char(related="password", string="Secret Access Key")
-    aws_region = fields.Selection(
-        selection=_get_aws_region, sparse="data", string="Region"
-    )
-    aws_cache_control = fields.Char(default="max-age=31536000, public")
