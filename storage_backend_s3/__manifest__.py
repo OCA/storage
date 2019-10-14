dbafa1ddf8e098c654c4be3169249ee821e4e636
Simone Orsi <simahawk@gmail.com>
2019-10-14 15:28:25 +0200

    [MIG] storage_backend_s3: Migration to 12.0

--- storage_backend_s3/__manifest__.py
+++ storage_backend_s3/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Akretion (http://www.akretion.com).
 # @author Sébastien BEAU <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -6,12 +5,12 @@
 {
     "name": "Storage Backend S3",
     "summary": "Implement amazon S3 Storage",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Storage",
     "website": "https://www.github.com/OCA/storage",
     "author": " Akretion, Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    "installable": False,
+    "installable": True,
     "external_dependencies": {"python": ["boto3"]},
     "depends": ["storage_backend"],
     "data": ["views/backend_storage_view.xml"],
