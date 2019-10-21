f736192c14060e9c43e64a521e0cad4f8f396781
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-10-21 14:25:18 +0200

    13.0: Create branche

--- storage_image_product/__manifest__.py
+++ storage_image_product/__manifest__.py
@@ -11,7 +11,7 @@
     "author": " Akretion, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "development_status": "Stable/Production",
-    "installable": True,
+    "installable": False,
     "depends": ["storage_image", "product", "sale"],  # only for the menu
     "data": [
         "security/ir.model.access.csv",
