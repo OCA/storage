c2d6f1fb5ac48544f540221935941c6ec55ca0d4
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-11-05 15:37:47 +0100

    [MIG] storage_image: Finalize migration to 13.0

--- storage_image/static/src/js/storage_image.js
+++ storage_image/static/src/js/storage_image.js
@@ -18,11 +18,12 @@ odoo.define('storage_image.image_url', function (require) {
             } else if (!url.startsWith('http')) {
                 url = 'data:image/png;base64,' + url;
             }
+
             /*
              From here it's the same code as in FieldBinaryImage
              */
             var $img = $(qweb.render("FieldBinaryImage-img", {widget: this, url: url}));
-            // override css size attributes (could have been defined in css files)
+            // Override css size attributes (could have been defined in css files)
             // if specified on the widget
             var width = this.nodeOptions.size ? this.nodeOptions.size[0] : this.attrs.width;
             var height = this.nodeOptions.size ? this.nodeOptions.size[1] : this.attrs.height;
