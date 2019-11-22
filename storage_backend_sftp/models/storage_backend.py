769314045c5f8a2db2f5fcaa8e485319f1812a68
Simone Orsi <simahawk@gmail.com>
2019-11-22 18:31:41 +0100

    Add server_env support

--- storage_backend_sftp/models/storage_backend.py
+++ storage_backend_sftp/models/storage_backend.py
@@ -1,5 +1,7 @@
 # Copyright 2017 Akretion (http://www.akretion.com).
 # @author Sébastien BEAU <sebastien.beau@akretion.com>
+# Copyright 2019 Camptocamp SA (http://www.camptocamp.com).
+# @author Simone Orsi <simone.orsi@camptocamp.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
@@ -19,3 +21,17 @@ class StorageBackend(models.Model):
         default="pwd",
         required=True,
     )
+
+    @property
+    def _server_env_fields(self):
+        env_fields = super()._server_env_fields
+        env_fields.update(
+            {
+                "sftp_password": {},
+                "sftp_login": {},
+                "sftp_server": {},
+                "sftp_port": {},
+                "sftp_auth_method": {},
+            }
+        )
+        return env_fields
