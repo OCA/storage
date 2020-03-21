fac4874efcc9ac1ebc1f3112ff9d66e0226d2f09
hparfr <raph@hpar.fr>
2020-03-21 16:04:29 +0100

    Improve the image view. Display linked products and categories.

---
+++ storage_image_product/models/storage_image.py
@@ -0,0 +1,18 @@
+# Copyright 2020 Akretion (http://www.akretion.com).
+# @author Raphaël Reverdy <raphael.reverdy@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from odoo import fields, models
+
+
+class StorageImage(models.Model):
+    _inherit = "storage.image"
+
+    image_relation_ids = fields.One2many(
+        "product.image.relation",
+        inverse_name="image_id", string="Products"
+    )
+    category_relation_ids = fields.One2many(
+        "category.image.relation",
+        inverse_name="image_id", string="Categories")
