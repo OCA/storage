85760327d4b5a9a77ce6a5952c6c65855e3819f6
François Honoré <francois.honore@acsone.eu>
2020-07-14 13:47:57 +0200

    [10.0][IMP] Improve the resequence (on JS + code) to apply the 'resequencing' directly on the parent (owner of the relation) instead of the relation itself. It's very useful to trigger correctly fields who depends on the relation (computed or related fields). We have the case for the image_medium_url on the product.template who is not re-computed when the image order is updated.

--- storage_image/static/src/js/image_handle.js
+++ storage_image/static/src/js/image_handle.js
@@ -3,8 +3,30 @@ odoo.define('storage_image.image_handle', function (require) {
     var core = require('web.core');
     var data = require('web.data');
     var Model = require("web.DataModel");
+    var session = require('web.session');
+    var pyeval = require('web.pyeval');
     var kanban = require("web_kanban.KanbanView");
     var m2m_kanban = require("web_kanban.Many2ManyKanbanView");
+
+    data.DataSet.include({
+        /* Inherit the DataSet to include a resequence on the parent record */
+        init : function() {
+            this._super.apply(this, arguments);
+        },
+
+        resequence_child: function (id, child_field, children_ids, options) {
+            options = options || {};
+            return session.rpc('/web/dataset/resequence_child', {
+                model: this.model,
+                target_id: id,
+                child_field: child_field,
+                children_ids: children_ids,
+                context: pyeval.eval('context', this.get_context(options.context)),
+            }).then(function (results) {
+                return results;
+            });
+        },
+    });
 
     core.view_registry.get("one2many_kanban").include({
         render: function () {
@@ -33,7 +55,14 @@ odoo.define('storage_image.image_handle', function (require) {
                     tolerance: 'pointer',
                     cursor: 'move',
                     update: function () {
-                        new data.DataSet(self, self.record_options.model).resequence(self._getIDs())
+                        // Only if we have info related to the parent
+                        if (self.dataset && self.dataset.parent_view.model && self.dataset.parent_view.datarecord && self.dataset.child_name) {
+                            new data.DataSet(self, self.dataset.parent_view.model).resequence_child(self.dataset.parent_view.datarecord.id, self.dataset.child_name, self._getIDs());
+                        }
+                        else {
+                            // If not, call the resequence on the relation record
+                            new data.DataSet(self, self.record_options.model).resequence(self._getIDs());
+                        }
                     },
                 });
             }
