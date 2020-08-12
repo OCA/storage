bcd8a54f42f51ff4b9b38db6241bc8d9f62c1f19
Simone Orsi <simahawk@gmail.com>
2020-08-12 08:59:04 +0200

    [MIG] storage_media: Migration to 13.0

--- storage_media/__manifest__.py
+++ storage_media/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Akretion (http://www.akretion.com).
 # @author Sébastien BEAU <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -6,13 +5,13 @@
 {
     "name": "Storage Media",
     "summary": "Give the posibility to store media data in Odoo",
-    "version": "10.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Uncategorized",
     "website": "https://www.github.com/OCA/storage",
     "author": " Akretion, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
-    "installable": False,
+    "installable": True,
     "external_dependencies": {"python": [], "bin": []},
     "depends": ["storage_file", "storage_thumbnail"],
     "data": [
