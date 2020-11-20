313b9301e2d35764f607895e591e02e8b50e3f03
Sébastien BEAU <sebastien.beau@akretion.com>
2020-11-20 10:31:22 +0100

    [FIX] fix wrong key in manisfest'

--- storage_image/__manifest__.py
+++ storage_image/__manifest__.py
@@ -10,7 +10,7 @@
     "website": "https://www.github.com/OCA/storage",
     "author": " Akretion, Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    "development_status": "Stable/Production",
+    "development_status": "Production/Stable",
     "installable": True,
     "depends": ["storage_thumbnail"],
     "external_dependencies": {"python": ["requests_mock"]},
