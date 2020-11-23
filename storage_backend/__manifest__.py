5470c309cd9eb14f0f115fe7b0666748e800f539
Simone Orsi <simahawk@gmail.com>
2020-11-23 08:55:17 +0100

    storage_backend bump 13.0.1.2.0

--- storage_backend/__manifest__.py
+++ storage_backend/__manifest__.py
@@ -5,12 +5,12 @@
 {
     "name": "Storage Bakend",
     "summary": "Implement the concept of Storage with amazon S3, sftp...",
-    "version": "13.0.1.1.1",
+    "version": "13.0.1.1.0",
     "category": "Storage",
     "website": "https://www.github.com/OCA/storage",
     "author": " Akretion, Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    "development_status": "Production/Stable",
+    "development_status": "Stable/Production",
     "installable": True,
     "depends": ["base", "component", "server_environment"],
     "data": [
