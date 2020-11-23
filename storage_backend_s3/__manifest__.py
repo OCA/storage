1f390c1aaba582e7cecfe0c99ffc9de100788f3d
Simone Orsi <simahawk@gmail.com>
2020-11-23 08:55:34 +0100

    storage_backend_s3 bump 13.0.1.1.0

--- storage_backend_s3/__manifest__.py
+++ storage_backend_s3/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Storage Backend S3",
     "summary": "Implement amazon S3 Storage",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.1.0",
     "category": "Storage",
     "website": "https://www.github.com/OCA/storage",
     "author": " Akretion, Odoo Community Association (OCA)",
