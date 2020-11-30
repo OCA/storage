7d6cf513bd2fe7f523a6bc6413b9575a39f0172e
Quentin Groulard <quentin.groulard@acsone.eu>
2020-11-30 15:12:21 +0100

    [ADD] Storage image product brand

---
+++ storage_image_product_brand/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2020 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Storage Image Product Brand",
+    "summary": """Link images to product brands""",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://www.github.com/OCA/storage",
+    "depends": ["storage_image", "product_brand"],
+    "data": [
+        "security/product_brand_image_relation.xml",
+        "views/product_brand.xml",
+        "views/product_brand_image_relation.xml",
+    ],
+}
