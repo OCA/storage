0438684e9dbdf38bf160edbf63888cbbfcd26fa9
Sébastien BEAU <sebastien.beau@akretion.com>
2020-12-06 10:17:45 +0100

    [MIG] batch migration of modules

--- storage_backend_s3/__manifest__.py
+++ storage_backend_s3/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Storage Backend S3",
     "summary": "Implement amazon S3 Storage",
-    "version": "13.0.1.1.0",
+    "version": "14.0.1.0.0",
     "category": "Storage",
     "website": "https://github.com/OCA/storage",
     "author": " Akretion, Odoo Community Association (OCA)",
