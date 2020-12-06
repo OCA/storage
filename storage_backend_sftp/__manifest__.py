619e0717a4d7b01c89fdf43c08f05fa81468ed22
Sébastien BEAU <sebastien.beau@akretion.com>
2020-12-06 10:10:22 +0100

    [IMP] all: black, isort, prettier

--- storage_backend_sftp/__manifest__.py
+++ storage_backend_sftp/__manifest__.py
@@ -10,7 +10,7 @@
     "website": "https://github.com/OCA/storage",
     "author": " Akretion,Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    "installable": False,
+    "installable": True,
     "external_dependencies": {"python": ["paramiko"]},
     "depends": ["storage_backend"],
     "data": ["views/backend_storage_view.xml"],
