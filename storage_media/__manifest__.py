e36ad8a4e9a30669b8521b45cc6217e3218f4502
Sébastien BEAU <sebastien.beau@akretion.com>
2020-12-06 09:53:40 +0100

    [ADD] add new V14 config

--- storage_media/__manifest__.py
+++ storage_media/__manifest__.py
@@ -11,7 +11,7 @@
     "author": " Akretion, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
-    "installable": True,
+    "installable": False,
     "external_dependencies": {"python": [], "bin": []},
     "depends": ["storage_file", "storage_thumbnail"],
     "data": [
