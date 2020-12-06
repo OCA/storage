f8de3ff75f042d67de5ef2cd26914cbcd0723ea4
Sébastien BEAU <sebastien.beau@akretion.com>
2020-12-06 10:17:45 +0100

    [MIG] batch migration of modules

--- storage_thumbnail/__manifest__.py
+++ storage_thumbnail/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Storage Thumbnail",
     "summary": "Abstract module that add the possibility to have thumbnail",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "category": "Storage",
     "website": "https://github.com/OCA/storage",
     "author": " Akretion, Odoo Community Association (OCA)",
