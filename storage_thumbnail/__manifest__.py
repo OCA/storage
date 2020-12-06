ebb5e25417fe3e1799e63e640400b4a9cded1cca
Sébastien BEAU <sebastien.beau@akretion.com>
2020-12-06 10:10:22 +0100

    [IMP] all: black, isort, prettier

--- storage_thumbnail/__manifest__.py
+++ storage_thumbnail/__manifest__.py
@@ -7,11 +7,11 @@
     "summary": "Abstract module that add the possibility to have thumbnail",
     "version": "13.0.1.0.1",
     "category": "Storage",
-    "website": "https://www.github.com/OCA/storage",
+    "website": "https://github.com/OCA/storage",
     "author": " Akretion, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "development_status": "Production/Stable",
-    "installable": False,
+    "installable": True,
     "depends": ["storage_file"],
     "data": [
         "data/ir_parameter.xml",
