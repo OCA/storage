9041251234e59838be97cacc2c2fc978b6e230e1
Sébastien BEAU <sebastien.beau@akretion.com>
2020-12-06 10:10:22 +0100

    [IMP] all: black, isort, prettier

--- storage_media/__manifest__.py
+++ storage_media/__manifest__.py
@@ -7,11 +7,11 @@
     "summary": "Give the posibility to store media data in Odoo",
     "version": "13.0.1.0.0",
     "category": "Uncategorized",
-    "website": "https://www.github.com/OCA/storage",
+    "website": "https://github.com/OCA/storage",
     "author": " Akretion, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
-    "installable": False,
+    "installable": True,
     "external_dependencies": {"python": [], "bin": []},
     "depends": ["storage_file", "storage_thumbnail"],
     "data": [
