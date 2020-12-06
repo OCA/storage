1895ffa91a646755068d81779f52c515832b2937
Sébastien BEAU <sebastien.beau@akretion.com>
2020-12-06 10:17:45 +0100

    [MIG] batch migration of modules

--- storage_file/__manifest__.py
+++ storage_file/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Storage File",
     "summary": "Storage file in storage backend",
-    "version": "13.0.1.2.0",
+    "version": "14.0.1.0.0",
     "category": "Storage",
     "website": "https://github.com/OCA/storage",
     "author": " Akretion, Odoo Community Association (OCA)",
