7bfbb47a516cbee641edd2dbca6b26f836cebd7c
Enric Tobella <etobella@creublanca.es>
2021-03-10 15:36:45 +0100

    [CHG] storage: Use more permissive licence: AGPL-> LGPL

--- storage_media/__manifest__.py
+++ storage_media/__manifest__.py
@@ -1,6 +1,6 @@
 # Copyright 2017 Akretion (http://www.akretion.com).
 # @author Sébastien BEAU <sebastien.beau@akretion.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     "name": "Storage Media",
@@ -9,7 +9,7 @@
     "category": "Uncategorized",
     "website": "https://github.com/OCA/storage",
     "author": " Akretion, Odoo Community Association (OCA)",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "application": False,
     "installable": True,
     "external_dependencies": {"python": [], "bin": []},
