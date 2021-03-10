00eed5738cfbe357ae46268d776b171230558d55
Enric Tobella <etobella@creublanca.es>
2021-03-10 15:36:45 +0100

    [CHG] storage: Use more permissive licence: AGPL-> LGPL

--- storage_backend_s3/tests/test_amazon_s3.py
+++ storage_backend_s3/tests/test_amazon_s3.py
@@ -1,6 +1,6 @@
 # Copyright 2017 Akretion (http://www.akretion.com).
 # @author Sébastien BEAU <sebastien.beau@akretion.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 # pylint: disable=missing-manifest-dependency
 # disable warning on 'vcr' missing in manifest: this is only a dependency for
