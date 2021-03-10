16abd064ede72a0e0fc7d109dd78064cfbc41323
Enric Tobella <etobella@creublanca.es>
2021-03-10 15:36:45 +0100

    [CHG] storage: Use more permissive licence: AGPL-> LGPL
    [UPD] README.rst

--- storage_backend/components/base_adapter.py
+++ storage_backend/components/base_adapter.py
@@ -1,6 +1,6 @@
 # Copyright 2017 Akretion (http://www.akretion.com).
 # @author Sébastien BEAU <sebastien.beau@akretion.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 # Copyright 2020 ACSONE SA/NV (<http://acsone.eu>)
 # @author Simone Orsi <simahawk@gmail.com>
 
