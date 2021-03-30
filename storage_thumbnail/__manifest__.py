88d77b50272d451b0eb6c1ad46c027b375914d5a
Simone Orsi <simahawk@gmail.com>
2021-03-30 16:35:58 +0200

    storage_thumb: do not store url
    The URL for all storage files is normally computed via backend.
    This ensures that if you have different conf by env
    you won't mess up w/ images coming from other envs (eg: prod).
    
    This change makes sure that the URL which is used for the thumbs
    is always computed by the backend.

--- storage_thumbnail/__manifest__.py
+++ storage_thumbnail/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Storage Thumbnail",
     "summary": "Abstract module that add the possibility to have thumbnail",
-    "version": "13.0.1.1.0",
+    "version": "13.0.1.2.0",
     "category": "Storage",
     "website": "https://github.com/OCA/storage",
     "author": " Akretion, Odoo Community Association (OCA)",
