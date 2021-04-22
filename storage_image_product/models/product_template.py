8d41b0255b95a09ea952ea59fdf3b916b8b3f257
Simone Orsi <simahawk@gmail.com>
2021-04-22 13:19:29 +0200

    storage_image_product: do not store images URLs
    Storing URLs is wrong because they depend on the backend.
    If backend rules change or environment changes
    URLs will be outdated.
    Instead, store the main image out of which we can compute the URLs.

--- storage_image_product/models/product_template.py
+++ storage_image_product/models/product_template.py
@@ -1,5 +1,7 @@
 # Copyright 2018 Akretion (http://www.akretion.com).
 # @author Raphaël Reverdy <https://github.com/hparfr>
+# Copyright 2021 Camptocamp SA (http://www.camptocamp.com)
+# @author Simone Orsi <simahawk@gmail.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 
@@ -13,23 +15,28 @@ _logger = logging.getLogger(__name__)
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
+    image_ids = fields.One2many(
+        comodel_name="product.image.relation",
+        inverse_name="product_tmpl_id",
+        string="Images",
+    )
+    main_image_id = fields.Many2one(
+        comodel_name="storage.image",
+        compute="_compute_main_image_id",
+        # Store it to improve perf on product views
+        store=True,
+    )
     # small and medium image are here to replace
     # native image field on form and kanban
-    image_small_url = fields.Char(compute="_compute_image_urls", store=True)
-    image_medium_url = fields.Char(compute="_compute_image_urls", store=True)
-    image_ids = fields.One2many(
-        "product.image.relation", inverse_name="product_tmpl_id", string="Images"
-    )
+    image_small_url = fields.Char(related="main_image_id.image_small_url")
+    image_medium_url = fields.Char(related="main_image_id.image_medium_url")
 
     @api.depends("image_ids", "image_ids.sequence", "image_ids.image_id")
-    def _compute_image_urls(self):
-        for template in self:
-            template_images = template.image_ids.sorted(
-                key=lambda i: (i.sequence, i.image_id)
-            )
-            if template_images:
-                template.image_small_url = template_images[0].image_id.image_small_url
-                template.image_medium_url = template_images[0].image_id.image_medium_url
-            else:
-                template.image_small_url = None
-                template.image_medium_url = None
+    def _compute_main_image_id(self):
+        for record in self:
+            record.main_image_id = record._get_main_image()
+
+    def _get_main_image(self):
+        return fields.first(
+            self.image_ids.sorted(key=lambda i: (i.sequence, i.image_id))
+        ).image_id
