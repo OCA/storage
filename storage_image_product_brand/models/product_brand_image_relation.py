b02b74cf56ed87f9fa25a4d47be3104c11685a0d
Ivàn Todorovich <ivan.todorovich@gmail.com>
2021-07-29 08:52:25 -0300

    [IMP] storage_image_product_brand: Technical improvements

--- storage_image_product_brand/models/product_brand_image_relation.py
+++ storage_image_product_brand/models/product_brand_image_relation.py
@@ -9,4 +9,8 @@ class ProductBrandImageRelation(models.Model):
     _inherit = "image.relation.abstract"
     _description = "Product Brand Image Relation"
 
-    brand_id = fields.Many2one("product.brand")
+    brand_id = fields.Many2one(
+        "product.brand",
+        required=True,
+        ondelete="cascade",
+    )
