319d02d5cb80a35c4c9ee67f8e514c132e83756a
Ivàn Todorovich <ivan.todorovich@gmail.com>
2021-07-29 16:34:44 -0300

    [IMP] storage_image_import: Tests
    Test didn't make any sense to me, and they were failing because
    of changes made in storage_image.
    
    I rewrote them in a way that makes sense, and removed the dependency
    to storage_image_product which was only here for tests I believe.

--- storage_image_import/tests/test_import.py
+++ storage_image_import/tests/test_import.py
@@ -1,126 +1,70 @@
 # Copyright 2020 Akretion (https://www.akretion.com).
+# Copyright 2021 Camptocamp (http://www.camptocamp.com).
 # @author Sébastien BEAU <sebastien.beau@akretion.com>
+# @author Iván Todorovich <ivan.todorovich@gmail.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-import base64
+
 import os
 import urllib
+from contextlib import contextmanager
 from unittest.mock import patch
 
 from odoo_test_helper import FakeModelLoader
 
-from odoo.addons.component.tests.common import SavepointComponentCase
+from odoo.addons.storage_image.tests.common import StorageImageCommonCase
 
 
-class TestImportImage(SavepointComponentCase):
+class ImportImageCase(StorageImageCommonCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-
-    def _import_image(self, vals):
-        path = os.path.dirname(os.path.abspath(__file__))
-        image = open(os.path.join(path, "static/oca-logo.png"), "rb")
-
-        with patch.object(urllib.request, "urlopen", return_value=image):
-            self.env["image.relation.abstract"]._process_import_from_url(vals)
-
-    def test_create(self):
-        vals = {
-            "import_from_url": ("https://www.exemple.com/product_images/oca-logo.png")
-        }
-        self._import_image(vals)
-        self.assertIn("image_id", vals)
-        self.env["storage.image"].browse(vals["image_id"]).data
-
-    def test_reimport(self):
-        vals = {
-            "import_from_url": ("https://www.exemple.com/product_images/oca-logo.png")
-        }
-        self._import_image(vals)
-        image_id = vals["image_id"]
-        vals = {
-            "import_from_url": ("https://www.exemple.com/product_images/oca-logo.png")
-        }
-        self._import_image(vals)
-        self.assertEqual(image_id, vals["image_id"])
-
-
-class ImportImageCase(SavepointComponentCase):
-    @classmethod
-    def setUpClass(cls):
-        super(ImportImageCase, cls).setUpClass()
+        # Load fake image relation model
         cls.loader = FakeModelLoader(cls.env, cls.__module__)
         cls.loader.backup_registry()
+        from .models.fake_image_relation import FakeImageRelation
 
-        # The fake class is imported here !! After the backup_registry
-        from .models import FakeProductImageRelation
-
-        cls.oca_image = cls._create_storage_image("akretion-logo.png")
-        cls.akretion_image = cls._create_storage_image("oca-logo.png")
-
-        cls.loader.update_registry((FakeProductImageRelation,))
-
-    @staticmethod
-    def _get_file_content(name, base_path=None, as_binary=False):
-        path = os.path.dirname(os.path.abspath(__file__))
-        with open(os.path.join(path, "static/", name), "rb") as f:
-            data = f.read()
-        if as_binary:
-            return data
-        return base64.b64encode(data)
+        cls.loader.update_registry((FakeImageRelation,))
+        # Create some permissions for our fake model
+        model = cls.env["ir.model"].search([("model", "=", "fake.image.relation")])
+        cls.env["ir.model.access"].sudo().create(
+            {
+                "name": "access.tester",
+                "model_id": model.id,
+                "perm_read": 1,
+                "perm_write": 1,
+                "perm_create": 1,
+                "perm_unlink": 1,
+            }
+        )
+        # Load test image
+        cls.base_path = os.path.dirname(os.path.abspath(__file__))
+        cls.oca_image = cls._create_storage_image_from_file("static/oca-logo.png")
+        cls.logo_image = cls._create_storage_image_from_file("static/akretion-logo.png")
+        # Some other data
+        cls.product_tmpl = cls.env.ref("product.product_product_4_product_template")
 
     @classmethod
-    def _create_storage_image(cls, name):
-        return cls.env["storage.image"].create(
-            {"name": name, "data": cls._get_file_content(name)}
-        )
+    def tearDownClass(cls):
+        cls.loader.restore_registry()
+        return super().tearDownClass()
 
-    def _create_multi_image(self, vals_list):
-        for vals in vals_list:
-            path = os.path.dirname(os.path.abspath(__file__))
-            if vals["image_id"] == self.oca_image.id:
-                image = open(os.path.join(path, "static/oca-logo.png"), "rb")
-            elif vals["image_id"] == self.akretion_image.id:
-                image = open(os.path.join(path, "static/akretion-logo.png"), "rb")
-            with patch.object(urllib.request, "urlopen", return_value=image):
-                self.env["product.image.relation"].create(vals_list)
+    @contextmanager
+    def _patched_request(self, filename, base_path=None):
+        path = base_path or self.base_path
+        with open(os.path.join(path, filename), "rb") as file:
+            with patch.object(urllib.request, "urlopen", return_value=file):
+                yield
 
-    def _id_present(self, img):
-        id_present = self.env["storage.image"].search([("id", "=", img.id)])
-        return id_present
-
-    def test_create_multi(self):
-        self.env["product.image.relation"].create(
-            [{"image_id": self.oca_image.id}, {"image_id": self.akretion_image.id}]
-        )
-
-        id_present = self._id_present(self.oca_image)
-        id2_present = self._id_present(self.akretion_image)
-        self.assertTrue(id_present.name, "oca-logo.png")
-        self.assertTrue(id2_present.name, "akretion-logo.png")
-        self.assertTrue(id_present.data)
-        self.assertTrue(id2_present.data)
-
-    def test_create_multi_with_import_from_url(self):
-        vals_list = [
-            {
-                "image_id": self.oca_image.id,
-                "import_from_url": (
-                    "https://www.exemple.com/product_images/oca-logo.png"
-                ),
-            },
-            {
-                "image_id": self.akretion_image.id,
-                "import_from_url": (
-                    "https://www.exemple.com/product_images/akretion-logo.png"
-                ),
-            },
-        ]
-
-        self._create_multi_image(vals_list)
-
-        id_present = self._id_present(self.oca_image)
-        id2_present = self._id_present(self.akretion_image)
-        self.assertTrue(id_present.name, "oca-logo.png")
-        self.assertTrue(id2_present.name, "akretion-logo.png")
-        self.assertTrue(id_present.data)
-        self.assertTrue(id2_present.data)
+    def test_import_from_url(self):
+        # Case 1: Create image from url
+        vals = {"import_from_url": "https://www.example.com/oca-logo.png"}
+        with self._patched_request("static/oca-logo.png"):
+            relation = self.env["fake.image.relation"].create(vals)
+        self.assertEqual(relation.image_id.name, self.oca_image.name)
+        self.assertEqual(relation.image_id.data, self.oca_image.data)
+        # Case 2: Write image from url
+        vals = {"import_from_url": "https://www.example.com/akretion-logo.png"}
+        with self._patched_request("static/akretion-logo.png"):
+            relation.write(vals)
+        self.assertEqual(relation.image_id.name, self.logo_image.name)
+        self.assertEqual(relation.image_id.data, self.logo_image.data)
