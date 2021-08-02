6a2de39002027c137e548ac268b7c1d98406727e
Ivàn Todorovich <ivan.todorovich@gmail.com>
2021-08-02 14:27:54 -0300

    [FIX] storage_image_product_brand: Do not store url
    The URL for all storage files is normally computed via backend.
    This ensures that if you have different conf by env
    you won't mess up w/ images coming from other envs (eg: prod).
    
    This change makes sure that the URL which is used for the thumbs
    is always computed by the backend.

--- storage_image_product_brand/models/product_brand.py
+++ storage_image_product_brand/models/product_brand.py
@@ -1,21 +1,35 @@
 # Copyright 2020 ACSONE SA/NV
+# Copyright 2021 Camptocamp (http://www.camptocamp.com).
+# @author Iván Todorovich <ivan.todorovich@gmail.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ProductBrand(models.Model):
-
     _inherit = "product.brand"
 
-    image_small_url = fields.Char(
-        related="image_ids.image_id.image_small_url", store=True
-    )
-    image_medium_url = fields.Char(
-        related="image_ids.image_id.image_medium_url", store=True
-    )
     image_ids = fields.One2many(
         comodel_name="product.brand.image.relation",
         inverse_name="brand_id",
         string="Images",
     )
+    main_image_id = fields.Many2one(
+        comodel_name="storage.image",
+        compute="_compute_main_image_id",
+        # Store it to improve perf on product views
+        store=True,
+    )
+    image_small_url = fields.Char(
+        string="Main Image URL (small)",
+        related="main_image_id.image_small_url"
+    )
+    image_medium_url = fields.Char(
+        string="Main Image URL (medium)",
+        related="main_image_id.image_medium_url"
+    )
+
+    @api.depends("image_ids.sequence")
+    def _compute_main_image_id(self):
+        for rec in self:
+            rec.main_image_id = fields.first(rec.image_ids)
