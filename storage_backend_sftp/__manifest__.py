5591ec39ddb1d36f1599a64ca7d3bed5f4b98f20
Iryna Vyshnevska <iryna.vyshnevska@camptocamp.com>
2021-12-06 00:37:04 +0200

    [MIG][15.0] storage_backend_sftp

--- storage_backend_sftp/__manifest__.py
+++ storage_backend_sftp/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Storage Backend SFTP",
     "summary": "Implement SFTP Storage",
-    "version": "14.0.2.0.1",
+    "version": "15.0.1.0.0",
     "category": "Storage",
     "website": "https://github.com/OCA/storage",
     "author": " Akretion,Odoo Community Association (OCA)",
