cc855eee3bb81643bfed6dd19576832246295767
Simone Orsi <simahawk@gmail.com>
2021-12-20 11:35:16 +0100

    s_backend_s3: refactor get bucket
    * avoid calling s3.meta.client.head_bucket on each get, because it's slow
    * rationalise implementation

--- storage_backend_s3/components/s3_adapter.py
+++ storage_backend_s3/components/s3_adapter.py
@@ -21,6 +21,10 @@ except ImportError as err:  # pragma: no cover
     _logger.debug(err)
 
 
+# Keep track of existing buckets and avoid checking for them every time.
+EXISTING_BUCKETS = []
+
+
 class S3StorageAdapter(Component):
     _name = "s3.adapter"
     _inherit = "base.storage.adapter"
@@ -45,30 +49,53 @@ class S3StorageAdapter(Component):
         params = self._aws_bucket_params()
         s3 = boto3.resource("s3", **params)
         bucket_name = self.collection.aws_bucket
-        bucket = s3.Bucket(bucket_name)
-        exists = True
+        bucket = self._get_or_create_bucket(s3, bucket_name, **params)
+        return bucket
+
+    def _get_or_create_bucket(self, s3, bucket_name, **params):
+        """Retrieve bucket object, create one if missing."""
+        exists = self._check_bucket_exists(s3, bucket_name)
+        if exists:
+            bucket = s3.Bucket(bucket_name)
+        else:
+            create_params = self._get_create_bucket_params(bucket_name, params)
+            bucket = s3.create_bucket(**create_params)
+        return bucket
+
+    def _check_bucket_exists(self, s3, bucket_name):
+        if bucket_name in EXISTING_BUCKETS:
+            # If the bucket is not available later
+            # we'll have an error on each call of course
+            # but that does not happen often
+            # and in any case you will get what's wrong soon.
+            return True
         try:
             s3.meta.client.head_bucket(Bucket=bucket_name)
+            # The call above is expensive, avoid it when possible.
+            EXISTING_BUCKETS.append(bucket_name)
+            return True
         except ClientError as e:
             # If a client error is thrown, then check that it was a 404 error.
             # If it was a 404 error, then the bucket does not exist.
             error_code = e.response["Error"]["Code"]
             if error_code == "404":
-                exists = False
+                msg = "Bucket not found"
+            else:
+                msg = str(e)
+            _logger.error(msg)
+            return False
         except EndpointConnectionError as error:
             # log verbose error from s3, return short message for user
             _logger.exception("Error during connection on S3")
             raise exceptions.UserError(str(error))
-        region_name = params.get("region_name")
-        if not exists:
-            if not region_name:
-                bucket = s3.create_bucket(Bucket=bucket_name)
-            else:
-                bucket = s3.create_bucket(
-                    Bucket=bucket_name,
-                    CreateBucketConfiguration={"LocationConstraint": region_name},
-                )
-        return bucket
+
+    def _get_create_bucket_params(self, bucket_name, params):
+        create_params = {"Bucket": bucket_name}
+        if params.get("region_name"):
+            create_params["CreateBucketConfiguration"] = {
+                "LocationConstraint": params["region_name"]
+            }
+        return create_params
 
     def _get_object(self, relative_path=None):
         bucket = self._get_bucket()
