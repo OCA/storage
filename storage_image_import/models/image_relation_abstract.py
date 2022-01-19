6c0c32a69ef2089ca15a0a0dfc16bf3f94535585
Sébastien BEAU <sebastien.beau@akretion.com>
2022-01-19 08:09:05 +0100

    [IMP] add the possibility to pass custom header, and read name from content disposition

--- storage_image_import/models/image_relation_abstract.py
+++ storage_image_import/models/image_relation_abstract.py
@@ -2,9 +2,11 @@
 # @author Sébastien BEAU <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
+import ast
 import base64
 import logging
 import os
+import re
 import urllib
 from urllib.parse import urlparse
 
@@ -19,16 +21,39 @@ class ImageRelationAbstract(models.AbstractModel):
 
     import_from_url = fields.Char(related="image_id.imported_from_url")
 
+    def _get_filename(self, headers, url):
+        # Note it will be better to use rfc6266 lib but it's doesn't support python 3.7
+        if headers["Content-Disposition"]:
+            fname = re.findall(
+                r"filename\*?=([^;]+)",
+                headers["Content-Disposition"],
+                flags=re.IGNORECASE,
+            )
+            fname = fname[0].strip().strip('"')
+        if fname:
+            return fname
+        else:
+            return os.path.basename(urlparse(url).path)
+
+    def _get_download_header(self):
+        headers = self.env["ir.config_parameter"].get_param(
+            "storage_image_import.headers", {}
+        )
+        return ast.literal_eval(headers)
+
+    def _prepare_image_from_url(self, url):
+        req = urllib.request.Request(url, headers=self._get_download_header())
+        res = urllib.request.urlopen(req)
+        data = res.read()
+        return {
+            "name": self._get_filename(res.headers, url),
+            "data": base64.b64encode(data),
+            "imported_from_url": url,
+        }
+
     def _create_image_from_url(self, url):
         try:
-            data = urllib.request.urlopen(url).read()
-            return self.env["storage.image"].create(
-                {
-                    "name": os.path.basename(urlparse(url).path),
-                    "data": base64.b64encode(data),
-                    "imported_from_url": url,
-                }
-            )
+            return self.env["storage.image"].create(self._prepare_image_from_url(url))
         except Exception as e:
             _logger.error(e)
             raise ValidationError(
