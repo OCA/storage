d9e9e8abbb2c3c08f42cea25555bd4fec9355e9a
Jasmin Solanki <jasmin.solanki@forgeflow.com>
2022-06-10 16:02:46 +0530

    [MIG] storage_thumbnail: Migration to 15.0

--- storage_thumbnail/__manifest__.py
+++ storage_thumbnail/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Storage Thumbnail",
     "summary": "Abstract module that add the possibility to have thumbnail",
-    "version": "14.0.2.3.0",
+    "version": "15.0.1.0.0",
     "category": "Storage",
     "website": "https://github.com/OCA/storage",
     "author": " Akretion, Odoo Community Association (OCA)",
