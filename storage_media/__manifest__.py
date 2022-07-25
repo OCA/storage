78cc51345c83d4c0fda15e3af28161a5da6da932
Jasmin Solanki <jasmin.solanki@forgeflow.com>
2022-07-25 14:26:12 +0530

    [MIG] storage_media: Migration to 15.0

--- storage_media/__manifest__.py
+++ storage_media/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Storage Media",
     "summary": "Give the posibility to store media data in Odoo",
-    "version": "14.0.2.1.0",
+    "version": "15.0.1.0.0",
     "category": "Uncategorized",
     "website": "https://github.com/OCA/storage",
     "author": " Akretion, Odoo Community Association (OCA)",
