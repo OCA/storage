e4dfe6f40b2d1b4b74324fd631e507b845f3d42c
Denis Roussel <denis.roussel@acsone.eu>
2022-10-18 17:52:24 +0200

    [IMP] storage_backend_s3: Limit boto3 version to fulfill requests freeze in Odoo

--- setup/storage_backend_s3/setup.py
+++ setup/storage_backend_s3/setup.py
@@ -1,3 +1,11 @@
 import setuptools
 
-setuptools.setup(setup_requires=["setuptools-odoo"], odoo_addon=True)
+setuptools.setup(
+    setup_requires=["setuptools-odoo"],
+    odoo_addon={
+        'external_dependencies_override': {
+            'python': {
+                'boto3': 'boto3<1.16',
+            },
+        }
+    },)
