771ebe3672e33dd4d3adfccc3d0408b525c14bd2
Simone Orsi <simahawk@gmail.com>
2023-03-06 13:44:15 +0100

    storage_backend_sftp: fix pin cryptography<37

--- storage_backend_sftp/__manifest__.py
+++ storage_backend_sftp/__manifest__.py
@@ -11,7 +11,15 @@
     "author": " Akretion,Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "installable": True,
-    "external_dependencies": {"python": ["paramiko"]},
+    # Place an upper bound on cryptography version to be compatible with
+    # pyopenssl 19 mentioned in Odoo 15's requirements.txt. If we don't do
+    # this, installing this module will try to upgrade cryptography to the latest
+    # version because the minimum required version in pysaml2 (>=3.1) is greater than
+    # version 2.6 (from Odoo's requirement.txt). Since cryptography/pyopenssl don't
+    # declare minimum supported versions, this lead to inconsistencies.
+    # https://github.com/OCA/server-auth/issues/424
+    # https://github.com/OCA/storage/pull/247
+    "external_dependencies": {"python": ["paramiko", "cryptography<37"]},
     "depends": ["storage_backend"],
     "data": ["views/backend_storage_view.xml"],
 }
