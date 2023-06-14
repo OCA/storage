798062b1497a621602a75b89dcc4213b9aaf53fe
Ruchir Shukla <ruchir@bizzappdev.com>
2023-06-14 16:07:01 +0530

    [MIG][15.0]storage_image_product_brand:Migration to 15.0

--- storage_image_product_brand/__manifest__.py
+++ storage_image_product_brand/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Storage Image Product Brand",
     "summary": "Link images to product brands",
-    "version": "14.0.2.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/storage",
