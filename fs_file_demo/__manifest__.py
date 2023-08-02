097807c2616cd3d121894963bef6f2ae5b4d4027
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2023-08-02 16:59:53 +0200

    [ADD] fs_file_demo: Demo addon for fs_file

---
+++ fs_file_demo/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Fs File Demo",
+    "summary": """Demo addon for fs_file""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/storage",
+    "depends": [
+        "fs_file",
+    ],
+    "data": [
+        "security/fs_file.xml",
+        "views/fs_file.xml",
+    ],
+    "demo": [],
+    "development_status": "Alpha",
+}
---
+++ fs_file_demo/readme/USAGE.rst
@@ -0,0 +1,2 @@
+Go into Settings > Technical > Fs File to create a new record of model
+``fs.file``, which contains a FSFile record.
