78535e0c488b540a898b373d2363b261fc60995d
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2023-09-03 15:43:14 +0200

    [ADD] fs_image: Field to store images
    Store your image into an external filesystem

---
+++ fs_image/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Fs Image",
+    "summary": """
+        Field to store images into filesystem storages""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/storage",
+    "depends": ["fs_file"],
+    "data": [],
+    "demo": [],
+    "maintainers": ["lmignon"],
+    "development_status": "Alpha",
+    "assets": {
+        "web.assets_backend": [
+            "fs_image/static/src/**/*",
+        ],
+    },
+}
