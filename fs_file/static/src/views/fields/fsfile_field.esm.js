3530b9d2621b3f85dc29aa2e96e1118b01c6ce09
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2023-09-03 15:37:46 +0200

    [IMP] fs_file: Improves UI
    Uses the FilUploader component to manage the file selection and save the file content at same time as the model. Prior to this change, the content was saved as soon as it was selected which is not possible on new records

--- fs_file/static/src/views/fields/fsfile_field.esm.js
+++ fs_file/static/src/views/fields/fsfile_field.esm.js
@@ -3,24 +3,20 @@
 /**
  * Copyright 2023 ACSONE SA/NV
  */
-
-import {registry} from "@web/core/registry";
-import {session} from "@web/session";
-import {formatFloat} from "@web/views/fields/formatters";
-import {useService} from "@web/core/utils/hooks";
-import {sprintf} from "@web/core/utils/strings";
-import {getDataURLFromFile} from "@web/core/utils/urls";
-import {standardFieldProps} from "@web/views/fields/standard_field_props";
 import {Component, onWillUpdateProps, useState} from "@odoo/owl";
 
-const DEFAULT_MAX_FILE_SIZE = 128 * 1024 * 1024; // 128MB
+import {FileUploader} from "@web/views/fields/file_handler";
+import {getDataURLFromFile} from "@web/core/utils/urls";
+import {registry} from "@web/core/registry";
+import {standardFieldProps} from "@web/views/fields/standard_field_props";
+import {useService} from "@web/core/utils/hooks";
 
 export class FSFileField extends Component {
     setup() {
         this.notification = useService("notification");
         this.state = useState({
             ...this.props.value,
-            isUploading: false,
+            isValid: true,
         });
         onWillUpdateProps((nextProps) => {
             this.state.isUploading = false;
@@ -29,34 +25,6 @@ export class FSFileField extends Component {
             this.state.mimetype = mimetype;
             this.state.url = url;
         });
-    }
-    get maxUploadSize() {
-        return session.max_file_upload_size || DEFAULT_MAX_FILE_SIZE;
-    }
-
-    edit() {
-        var input = document.createElement("input");
-        input.type = "file";
-        input.accept = this.props.acceptedFileExtensions;
-        input.onchange = (e) => {
-            const file = e.target.files[0];
-            if (file) {
-                if (file.size > this.maxUploadSize) {
-                    this.notification.add(
-                        sprintf(
-                            this.env._t(
-                                "The file size exceeds the maximum allowed size of %s MB."
-                            ),
-                            formatFloat(this.maxUploadSize / 1024 / 1024)
-                        ),
-                        {type: "danger"}
-                    );
-                    return;
-                }
-                this.uploadFile(file);
-            }
-        };
-        input.click();
     }
 
     async uploadFile(file) {
@@ -74,9 +42,30 @@ export class FSFileField extends Component {
     clear() {
         this.props.record.update({[this.props.name]: false});
     }
+
+    onFileRemove() {
+        this.state.isValid = true;
+        this.props.update(false);
+    }
+    onFileUploaded(info) {
+        this.state.isValid = true;
+        this.props.update({
+            filename: info.name,
+            content: info.data,
+        });
+    }
+    onLoadFailed() {
+        this.state.isValid = false;
+        this.notification.add(this.env._t("Could not display the selected image"), {
+            type: "danger",
+        });
+    }
 }
 
 FSFileField.template = "fs_file.FSFileField";
+FSFileField.components = {
+    FileUploader,
+};
 FSFileField.props = {
     ...standardFieldProps,
     acceptedFileExtensions: {type: String, optional: true},
