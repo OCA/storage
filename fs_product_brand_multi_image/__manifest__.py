da9a84efbe8a068f41ac44908fa364021b42c8d5
chien <chien@trobz.com>
2024-03-04 10:15:47 +0700

    [MIG] fs_product_brand_multi_image: Migration to 17.0

--- fs_product_brand_multi_image/__manifest__.py
+++ fs_product_brand_multi_image/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Fs Product Brand Multi Image",
     "summary": """
         Link images to product brands""",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/storage",
