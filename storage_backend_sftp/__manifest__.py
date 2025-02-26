38f5677eec05497105a22e3c5a29bc119e9073b2
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com>
2025-02-25 19:04:58 -0600

    [MIG] storage_backend_sftp: Migration to 17.0

--- storage_backend_sftp/__manifest__.py
+++ storage_backend_sftp/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Storage Backend SFTP",
     "summary": "Implement SFTP Storage",
-    "version": "16.0.1.0.1",
+    "version": "17.0.1.0.0",
     "category": "Storage",
     "website": "https://github.com/OCA/storage",
     "author": " Akretion,Odoo Community Association (OCA)",
