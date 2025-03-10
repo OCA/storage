5088d041618a341fc57fd9922089f6b54e226bff
Tobias Sorg <tobias.sorg@ametras.com>
2025-03-10 16:07:55 +0100

    [FIX] fs_storage: Fixed issue with fsspec version 2025.3.0

--- fs_storage/__manifest__.py
+++ fs_storage/__manifest__.py
@@ -19,5 +19,5 @@
         "wizards/fs_test_connection.xml",
     ],
     "demo": ["demo/fs_storage.xml"],
-    "external_dependencies": {"python": ["fsspec>=2024.5.0"]},
+    "external_dependencies": {"python": ["fsspec>=2024.5.0,<2025.3.0"]},
 }
