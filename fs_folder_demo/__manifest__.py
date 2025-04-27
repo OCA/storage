6dad2d4c5c024d325bf26217f8b6cd03f25ea2a7
Enric Tobella <enric.tobella@dixmit.com>
2025-04-27 15:32:37 +0200

    [ADD] fs_field_demo

---
+++ fs_folder_demo/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2025 Dixmit
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Fs Field Demo",
+    "summary": """Demo for FS Field""",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Dixmit,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/fs-content",
+    "depends": [
+        "fs_field",
+    ],
+    "data": [
+        "views/res_partner.xml",
+    ],
+    "demo": [],
+}
