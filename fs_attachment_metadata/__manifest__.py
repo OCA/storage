f1302035214415320d8b6b76629c755503109ea5
Alexis Yushin <alexis@apexive.com>
2025-06-17 13:38:55 -0400

    set storage metadata action

--- fs_attachment_metadata/__manifest__.py
+++ fs_attachment_metadata/__manifest__.py
@@ -11,14 +11,17 @@
         - Automatic content-type setting on file creation
         - Metadata updates when mimetype changes
         - Support for GCS and other storage backends with metadata capabilities
+        - Mass action to set content-type metadata on existing attachments
     """,
     "author": "Apexive Solutions LLC",
     "website": "https://github.com/apexive/odoo-storage",
     "category": "Technical",
     "version": "16.0.1.0.0",
     "depends": ["fs_attachment"],
+    "data": [
+        "data/ir_actions_server.xml",
+    ],
     "license": "AGPL-3",
     "installable": True,
     "auto_install": False,
 }
-
