c2132fb3e3daef11ba80c14c9de1b51d84528cc0
Sébastien BEAU <sebastien.beau@akretion.com>
2025-09-23 15:25:21 +0200

    [FIX] do not prefetch data on image, file.

--- storage_image/models/storage_image.py
+++ storage_image/models/storage_image.py
@@ -30,6 +30,11 @@ class StorageImage(models.Model):
     alt_name = fields.Char(string="Alt Image name")
     file_id = fields.Many2one("storage.file", "File", required=True, ondelete="cascade")
 
+    def _in_cache_without(self, field, limit=models.PREFETCH_MAX):
+        if field.name == "data":
+            limit = 1
+        return super()._in_cache_without(field, limit=limit)
+
     @api.onchange("name")
     def onchange_name(self):
         for record in self:
