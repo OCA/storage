fd1c61816d7dfaf522da06342a625a8f8bd24ac6
Souheil Bejaoui <souheil.bejaoui@acsone.eu>
2026-03-11 13:56:41 +0100

    [MIG] fs_storage_ms_drive: migration to 19.0

--- fs_storage_ms_drive/__manifest__.py
+++ fs_storage_ms_drive/__manifest__.py
@@ -4,13 +4,12 @@
 {
     "name": "Filesystem Storage For Microsoft Drives",
     "summary": "Add the microsoft drives (OneDrive, Sharepoint) as a storage backend",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "FS Storage",
     "website": "https://github.com/OCA/storage",
     "author": " ACSONE SA/NV, Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "development_status": "Beta",
-    "installable": False,
     "depends": [
         "microsoft_drive_account",
         "fs_storage",
