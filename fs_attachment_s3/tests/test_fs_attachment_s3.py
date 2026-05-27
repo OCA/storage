491930889453477bcf862b05da86db65167acd4f
Jos De Graeve <jos.degraeve@accomodata.be>
2026-05-27 12:38:57 +0200

    [IMP] fs_attachment_s3: Allow to use a prefix path and bucket in the directory_path

--- fs_attachment_s3/tests/test_fs_attachment_s3.py
+++ fs_attachment_s3/tests/test_fs_attachment_s3.py
@@ -33,3 +33,37 @@ class TestFSAttachementS3(TestFSAttachmentS3Common):
             "/fs_x_sendfile/http/minio.minio/test-bucket/dir/sub/fake_s3_file.txt",
             f"The X-Accel-Redirect path should match the expected format. ({url})",
         )
+
+    def test_get_x_sendfile_path_s3_signed_with_prefix(self):
+        """Test the path generation when the directory path has a prefix."""
+        self.s3_backend.write(
+            {
+                "s3_uses_signed_url_for_x_sendfile": True,
+                "s3_signed_url_expiration": 60,
+                "directory_path": "test-bucket/test-prefix",
+            }
+        )
+
+        url = self.fake_attachment_s3._get_x_sendfile_path()
+        self.assertTrue(
+            url.startswith(
+                "/fs_x_sendfile/http/minio.minio/"
+                "test-bucket/test-prefix/dir/sub/fake_s3_file.txt?"
+            ),
+            f"The URL should include the prefix. ({url})",
+        )
+
+    def test_get_x_sendfile_path_with_prefix(self):
+        """Test the unsigned path generation with a prefix."""
+        self.s3_backend.write(
+            {
+                "s3_uses_signed_url_for_x_sendfile": False,
+                "directory_path": "test-bucket/test-prefix",
+            }
+        )
+        url = self.fake_attachment_s3._get_x_sendfile_path()
+        self.assertEqual(
+            url,
+            "/fs_x_sendfile/http/minio.minio/test-bucket/test-prefix/dir/sub/fake_s3_file.txt",
+            f"The URL should include the prefix. ({url})",
+        )
