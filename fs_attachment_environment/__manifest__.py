763dfd93398d5eb219b47357deeb13dbeeca9de9
Enric Tobella <enric.tobella@dixmit.com>
2026-06-03 18:32:01 +0200

    [ADD] fs_attachment_environment

---
+++ fs_attachment_environment/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2026 Dixmit
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Filesystem Attachment Backend",
+    "summary": "Allows to use server environment with fs storage attachment",
+    "version": "17.0.1.0.0",
+    "category": "FS Storage",
+    "website": "https://github.com/OCA/storage",
+    "author": " ACSONE SA/NV, Dixmit, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "development_status": "Beta",
+    "installable": True,
+    "depends": ["fs_storage_environment", "fs_attachment"],
+}
