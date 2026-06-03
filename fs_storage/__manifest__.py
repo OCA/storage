51bdcf25902715aaa17b0bc11076dda700c83824
Enric Tobella <enric.tobella@dixmit.com>
2026-06-03 16:12:10 +0200

    [IMP] fs_storage: Remove dependancy on server_environment

--- fs_storage/__manifest__.py
+++ fs_storage/__manifest__.py
@@ -5,14 +5,14 @@
 {
     "name": "Filesystem Storage Backend",
     "summary": "Implement the concept of Storage with amazon S3, sftp...",
-    "version": "16.0.1.5.0",
+    "version": "16.0.2.0.0",
     "category": "FS Storage",
     "website": "https://github.com/OCA/storage",
     "author": " ACSONE SA/NV, Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "development_status": "Beta",
     "installable": True,
-    "depends": ["base", "base_sparse_field", "server_environment"],
+    "depends": ["base", "base_sparse_field"],
     "data": [
         "views/fs_storage_view.xml",
         "security/ir.model.access.csv",
--- fs_storage/readme/CONTRIBUTORS.rst
+++ fs_storage/readme/CONTRIBUTORS.rst
@@ -1,2 +1,3 @@
 * Laurent Mignon <laurent.mignon@acsone.eu>
 * Sébastien BEAU <sebastien.beau@akretion.com>
+* Enric Tobella
