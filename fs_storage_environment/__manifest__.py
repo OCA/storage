8ab22a789b10de20a1370751401decc7651e989f
Enric Tobella <enric.tobella@dixmit.com>
2026-06-03 16:12:24 +0200

    [ADD] fs_storage_environment

---
+++ fs_storage_environment/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2026 Dixmit
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Filesystem Storage Backend",
+    "summary": "Allows to use server environment with fs storage",
+    "version": "17.0.1.0.0",
+    "category": "FS Storage",
+    "website": "https://github.com/OCA/storage",
+    "author": " ACSONE SA/NV, Dixmit, Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "development_status": "Beta",
+    "installable": True,
+    "depends": ["fs_storage", "server_environment"],
+}
