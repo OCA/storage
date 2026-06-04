53e7be8e7dba76e238e1e3eb8edb9cf1bcdfc8e4
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2026-06-04 14:21:49 +0200

    [FIX] fs_attachment: Add migration script for server_environment split

--- fs_attachment/__manifest__.py
+++ fs_attachment/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Base Attachment Object Store",
     "summary": "Store attachments on external object store",
-    "version": "19.0.1.1.2",
+    "version": "19.0.1.1.3",
     "author": "Camptocamp, ACSONE SA/NV, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "development_status": "Beta",
