a048940a9453a8d63ce1a0a309017d68868bdb21
Akim Juillerat <akim.juillerat@camptocamp.com>
2026-07-10 23:17:03 +0200

    Copy fs_attachment_s3 into fs_attachment_azure

---
+++ fs_attachment_azure/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2025 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Fs Attachment S3",
+    "summary": """Store attachments into S3 complient filesystem""",
+    "version": "17.0.1.2.1",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/storage",
+    "depends": ["fs_attachment"],
+    "external_dependencies": {
+        "python": [
+            "fsspec[s3]",
+        ],
+    },
+    "data": [
+        "views/fs_storage.xml",
+    ],
+    "maintainers": ["lmignon"],
+}
