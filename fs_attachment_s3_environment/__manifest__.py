63bcab13a8ff98306b10eb7704e19de49eab07ca
Maksym Yankin <yankinmk@gmail.com>
2026-08-19 15:23:48 +0300

    [IMP] fs_attachment_s3_environment: preserve data on install and restore columns on uninstall

--- fs_attachment_s3_environment/__manifest__.py
+++ fs_attachment_s3_environment/__manifest__.py
@@ -14,4 +14,6 @@
     "depends": ["fs_attachment_environment", "fs_attachment_s3"],
     "data": [],
     "auto_install": True,
+    "post_init_hook": "post_init_hook",
+    "uninstall_hook": "uninstall_hook",
 }
