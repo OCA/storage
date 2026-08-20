eb62422eb49889ae6bbea9bcda43903a7f9d9261
Maksym Yankin <yankinmk@gmail.com>
2026-08-20 13:11:09 +0300

    [IMP] storage_backend_s3_environment: preserve data on install and restore columns on uninstall

--- storage_backend_s3_environment/__manifest__.py
+++ storage_backend_s3_environment/__manifest__.py
@@ -12,4 +12,6 @@
     "development_status": "Beta",
     "installable": True,
     "depends": ["storage_backend_environment", "storage_backend_s3"],
+    "post_init_hook": "post_init_hook",
+    "uninstall_hook": "uninstall_hook",
 }
