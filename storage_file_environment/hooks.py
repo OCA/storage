4887db5d02e51b6e670dce96ea4b075250725222
Maksym Yankin <yankinmk@gmail.com>
2026-08-20 13:14:29 +0300

    [IMP] storage_file_environment: preserve data on install and restore columns on uninstall

---
+++ storage_file_environment/hooks.py
@@ -0,0 +1,24 @@
+# Copyright 2026 Camptocamp SA
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo.addons.server_environment.uninstall import restore_env_managed_columns
+
+ENV_MANAGED_FIELDS = [
+    "filename_strategy",
+    "served_by",
+    "base_url",
+    "url_include_directory_path",
+]
+
+
+def post_init_hook(env):
+    env["storage.backend"]._preserve_not_env_managed_data(ENV_MANAGED_FIELDS)
+
+
+def uninstall_hook(env):
+    """Restore database columns dropped by server.env.mixin."""
+    restore_env_managed_columns(
+        env,
+        "storage.backend",
+        ENV_MANAGED_FIELDS,
+    )
