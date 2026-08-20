6110b2f7e0406b3912183bab2502335371b96fcb
Maksym Yankin <yankinmk@gmail.com>
2026-08-20 11:10:08 +0300

    [ADD] storage_backend_environment

---
+++ storage_backend_environment/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2026 Camptocamp SA
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Storage Backend Environment",
+    "summary": "Allows to use server environment with storage backend",
+    "version": "18.0.1.0.0",
+    "category": "Storage",
+    "website": "https://github.com/OCA/storage",
+    "author": " Akretion, Camptocamp, Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "development_status": "Beta",
+    "installable": True,
+    "depends": ["storage_backend", "server_environment"],
+    "auto_install": True,
+}
