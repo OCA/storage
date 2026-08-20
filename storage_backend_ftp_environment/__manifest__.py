f8f19590b7a4526083a3b4aa0517dedf02be8c7e
Maksym Yankin <yankinmk@gmail.com>
2026-08-20 13:08:11 +0300

    [IMP] storage_backend_ftp_environment: preserve data on install and restore columns on uninstall

--- storage_backend_ftp_environment/__manifest__.py
+++ storage_backend_ftp_environment/__manifest__.py
@@ -12,5 +12,7 @@
     "development_status": "Beta",
     "installable": True,
     "depends": ["storage_backend_environment", "storage_backend_ftp"],
+    "post_init_hook": "post_init_hook",
+    "uninstall_hook": "uninstall_hook",
     "auto_install": True,
 }
