8e3c00e11a9c58171ac2af8bd04391f08de0f8c2
Maksym Yankin <yankinmk@gmail.com>
2026-08-20 11:12:15 +0300

    [ADD] storage_backend_sftp_environment

---
+++ storage_backend_sftp_environment/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2026 Camptocamp SA
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Storage Backend SFTP Environment",
+    "summary": "Allows to use server environment with storage backend SFTP",
+    "version": "18.0.1.0.0",
+    "category": "Storage",
+    "website": "https://github.com/OCA/storage",
+    "author": " Akretion, Camptocamp, Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "development_status": "Beta",
+    "installable": True,
+    "depends": ["storage_backend_environment", "storage_backend_sftp"],
+}
---
+++ storage_backend_sftp_environment/readme/CONTRIBUTORS.md
@@ -0,0 +1,2 @@
+- Sébastien BEAU \<sebastien.beau@akretion.com\>
+- Simone Orsi
