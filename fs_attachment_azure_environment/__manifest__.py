efdb1cf11c4d47c04570f18a33e46a60be86b041
Iván Todorovich <ivan.todorovich@gmail.com>
2026-09-24 12:02:37 -0300

    [MIG] fs_attachment_azure_environment: Migration to 18.0

--- fs_attachment_azure_environment/__manifest__.py
+++ fs_attachment_azure_environment/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Filesystem Attachment Backend Azure",
     "summary": "Allows to use server environment with fs storage attachment Azure",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "FS Storage",
     "website": "https://github.com/OCA/storage",
     "author": "ACSONE SA/NV,Camptocamp,Odoo Community Association (OCA)",
